707c01a31a7ed6384fe98d8c8c0e36fd7e9633e6
Matias Dahl <matias.dahl@iki.fi>
2022-02-27 17:32:12 +0200

    move static data to separate js-file

--- mlflow/server/js/src/experiment-tracking/static-data/StaticMlflowService.js
+++ mlflow/server/js/src/experiment-tracking/static-data/StaticMlflowService.js
@@ -1,127 +1,8 @@
-const LIST_EXPERIMENTS_API_STATIC_RESPONSE = {
-  experiments: [{
-      experiment_id: '0',
-      name: 'Experiment #0',
-      artifact_location: './mlruns/0',
-      lifecycle_stage: 'active',
-      tags: [{
-        key: 'mlflow.note.content',
-        value: 'Description shown when opening experiment page. #0',
-      }, ],
-    },
-    {
-      experiment_id: '1',
-      name: 'My experiment #1',
-      artifact_location: './mlruns/1',
-      lifecycle_stage: 'active',
-      tags: [{
-        key: 'mlflow.note.content',
-        value: 'Description shown when opening experiment page. #1',
-      }, ],
-    },
-    {
-      experiment_id: '2',
-      name: 'Last experiment #2',
-      artifact_location: './mlruns/2',
-      lifecycle_stage: 'active',
-    },
-  ]
-};
+import {
+  STATIC_EXPERIMENTS,
+  STATIC_RUNS
+} from './StaticData';
 
-
-const ALL_RUNS = [{
-    info: {
-      run_uuid: "00000000000000000000000000000000",
-      experiment_id: "0",
-      user_id: "root",
-      status: "FINISHED",
-      start_time: 1645952322527,
-      end_time: 1645952322545,
-      artifact_uri: "/repo-root/backend/artefacts/0/00000000000000000000000000000000/artifacts",
-      lifecycle_stage: "active",
-      run_id: "00000000000000000000000000000000"
-    },
-    data: {
-      tags: [{
-          key: "mlflow.user",
-          value: "root"
-        },
-        {
-          key: "mlflow.source.name",
-          value: "/path/to/my/main-file.py"
-        },
-        {
-          key: "mlflow.source.type",
-          value: "LOCAL"
-        },
-        {
-          key: "mlflow.source.git.commit",
-          value: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-        },
-        {
-          key: "mlflow.runName",
-          value: "my-run-name"
-        },
-        {
-          key: "foo",
-          value: "bar"
-        },
-        {
-          key: "mlflow.note.content",
-          value: "this is a run description"
-        }
-      ]
-    }
-  },
-  {
-    info: {
-      run_uuid: "00000000000000000000000000000111",
-      experiment_id: "0",
-      user_id: "root",
-      status: "FINISHED",
-      start_time: 1645952322527,
-      end_time: 1645952322545,
-      artifact_uri: "/repo-root/backend/artefacts/0/00000000000000000000000000000111/artifacts",
-      lifecycle_stage: "active",
-      run_id: "00000000000000000000000000000111"
-    },
-    data: {
-      tags: [{
-          key: "mlflow.user",
-          value: "root"
-        },
-        {
-          key: "mlflow.source.name",
-          value: "/path/to/my/source-file.py"
-        },
-        {
-          key: "mlflow.source.type",
-          value: "LOCAL"
-        },
-        {
-          key: "mlflow.source.git.commit",
-          value: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-        },
-        {
-          key: "mlflow.parentRunId",
-          value: "00000000000000000000000000000000"
-        },
-        {
-          key: "mlflow.runName",
-          value: "my-run-name"
-        },
-        {
-          key: "foo",
-          value: "bar"
-        },
-        {
-          key: "mlflow.note.content",
-          value: "this is a run description"
-        }
-      ]
-    }
-  }
-];
 
 const one = (xs) => {
   if (xs.length === 1) {
@@ -133,7 +14,7 @@ const one = (xs) => {
 
 export class StaticMlflowService {
   static listExperiments(dummy_arg) {
-    return new Promise((resolve, reject) => resolve(LIST_EXPERIMENTS_API_STATIC_RESPONSE));
+    return new Promise((resolve, reject) => resolve(STATIC_EXPERIMENTS));
   };
 
   static getExperiment({
@@ -141,7 +22,7 @@ export class StaticMlflowService {
   }) {
     return new Promise((resolve, reject) => {
       resolve(
-        LIST_EXPERIMENTS_API_STATIC_RESPONSE
+        STATIC_EXPERIMENTS
         .experiments
         .filter((entry) => entry.experiment_id === experiment_id))
     }).then(xs => ({
@@ -154,7 +35,7 @@ export class StaticMlflowService {
   }) {
     return new Promise((resolve, reject) => {
       resolve(
-        ALL_RUNS.filter((entry) => entry.info.run_id === run_id))
+        STATIC_RUNS.filter((entry) => entry.info.run_id === run_id))
     }).then(xs => ({
       run: one(xs)
     }))
@@ -164,6 +45,7 @@ export class StaticMlflowService {
     run_uuid,
     path
   }) {
+    // TOOO: implement ARTEFACT_LIST_PER_STATIC_RUN etc with static hosted content
     return new Promise((resolve, reject) => resolve([]));
   }
 
@@ -179,7 +61,7 @@ export class StaticMlflowService {
       return new Promise((resolve, reject) => reject(new Error("Load more not supported in static mode.")));
     }
 
-    const filteredRuns = ALL_RUNS.filter(runEntry => experiment_ids.includes(runEntry.info.experiment_id));
+    const filteredRuns = STATIC_RUNS.filter(runEntry => experiment_ids.includes(runEntry.info.experiment_id));
 
     return new Promise((resolve, reject) => {
       resolve({

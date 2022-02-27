85109d272e0fe231da0deb59ef0d2897de27b01d
Matias Dahl <matias.dahl@iki.fi>
2022-02-27 18:19:06 +0200

    Add `HOST_STATIC_SITE` env-flag for hosting static read-only MLFlow website (#3)
    * update makefile docs; add start-sqlite-web-ui task
    
    * add HOST_STATIC_SITE env-flag
    
    * static versions of Experiment-level GET requests
    
    * support run-page, with listArtefacts always empty
    
    * 1st version of static version of searchRuns
    
    * return no models in static setup
    
    * move static data to separate js-file
    
    * code cleanup

---
+++ mlflow/server/js/src/experiment-tracking/static-data/StaticMlflowService.js
@@ -0,0 +1,72 @@
+import {
+  STATIC_EXPERIMENTS,
+  STATIC_RUNS
+} from './StaticData';
+
+
+const one = (xs) => {
+  if (xs.length === 1) {
+    return xs[0];
+  } else {
+    throw new Error("Expected one result; found ${xs.length}");
+  }
+};
+
+export class StaticMlflowService {
+  static listExperiments(dummy_arg) {
+    return new Promise((resolve, reject) => resolve(STATIC_EXPERIMENTS));
+  };
+
+  static getExperiment({
+    experiment_id
+  }) {
+    return new Promise((resolve, reject) => {
+      resolve(
+        STATIC_EXPERIMENTS
+        .experiments
+        .filter((entry) => entry.experiment_id === experiment_id))
+    }).then(xs => ({
+      experiment: one(xs)
+    }))
+  }
+
+  static getRun({
+    run_id
+  }) {
+    return new Promise((resolve, reject) => {
+      resolve(
+        STATIC_RUNS.filter((entry) => entry.info.run_id === run_id))
+    }).then(xs => ({
+      run: one(xs)
+    }))
+  }
+
+  static listArtifacts({
+    run_uuid,
+    path
+  }) {
+    // TOOO: implement ARTEFACT_LIST_PER_STATIC_RUN etc with static hosted content
+    return new Promise((resolve, reject) => resolve([]));
+  }
+
+  static searchRuns({
+    experiment_ids,
+    filter, // ignore for now (TODO?); eg. "", undefined
+    run_view_type, // ignore, can be "ACTIVE_ONLY" or undefined
+    max_results, // ignore
+    order_by, // ignore for now (TODO); eg.  ["attributes.start_time DESC"]
+    page_token, // assumed always NULL (max_results not respected)
+  }) {
+    if (!!page_token) {
+      return new Promise((resolve, reject) => reject(new Error("Load more not supported in static mode.")));
+    }
+
+    const filteredRuns = STATIC_RUNS.filter(runEntry => experiment_ids.includes(runEntry.info.experiment_id));
+
+    return new Promise((resolve, reject) => {
+      resolve({
+        runs: filteredRuns
+      });
+    });
+  }
+}

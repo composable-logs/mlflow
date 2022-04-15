db1edccb5a2308ab9dfd883c7365df40c7f11b10
Matias Dahl <matias.dahl@iki.fi>
2022-04-15 16:47:11 +0300

    render entire artifile filename w path

--- mlflow/server/js/src/experiment-tracking/components/ArtifactView.js
+++ mlflow/server/js/src/experiment-tracking/components/ArtifactView.js
@@ -92,7 +92,7 @@ export class ArtifactViewImpl extends Component {
               description='Label to display the full path of where the artifact of the experiment runs is located'
             />
           </label>{' '}
-          <Text className='artifact-info-text' ellipsis copyable>
+          <Text className='artifact-info-text' copyable>
             {activeNodeRealPath}
           </Text>
         </div>

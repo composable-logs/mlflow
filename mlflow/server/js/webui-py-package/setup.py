00c39663dc9b285819a7b19bf199c557222520f1
Matias Dahl <matias.dahl@iki.fi>
2022-08-03 17:25:17 +0300

    setup.py update

--- mlflow/server/js/webui-py-package/setup.py
+++ mlflow/server/js/webui-py-package/setup.py
@@ -1,16 +1,30 @@
+from pathlib import Path
+
 from setuptools import setup, find_packages
 
 PACKAGE_NAME = "pynb_dag_runner_webui"
+PACKAGE_VERSION = "0.0.0"
+
+# -- test that website.zip is in the same directory as setup.py
+if not (Path(__file__).parent / "website.zip").is_file():
+    raise Exception(f"Please add website.zip before building {PACKAGE_NAME}!")
 
 setup(
     name=PACKAGE_NAME,
-    author="Matias Dahl (based on modified version of the MLFlow project)",
+    author="Matias Dahl (based on a modified version of the MLFlow project)",
+    description=(
+        "This Python package contain compiled Javascript code from "
+        "various sources and with different licenses; "
+        "For details, please see the linked documentation and source repo "
+        "https://github.com/pynb-dag-runner/mlflow and "
+        "https://github.com/pynb-dag-runner/mlflow/pull/2"
+    ),
     author_email="matias.dahl@iki.fi",
-    license="TODO",
+    license="Various",
     classifiers=[],
     url="https://pynb-dag-runner.github.io/pynb-dag-runner/",
-    version="0.0.0",
+    version=PACKAGE_VERSION,
     packages=find_packages(),
-    # file paths relative to package root
-    package_data={PACKAGE_NAME: ['data-file.txt']},
+    # file paths relative to setup.py location
+    data_files=[('assets', ["website.zip"])],
 )

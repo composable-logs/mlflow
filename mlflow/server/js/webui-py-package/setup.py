b6c17a5ffa04516796a4d022c914ac08f820e969
Matias Dahl <matias.dahl@iki.fi>
2022-08-05 19:35:39 +0300

    Add GHA to build Python package with compiled front UI static site (#32)
    * add dir structure + initial makefiles
    
    * copy base Dockerfile from pynb-dag-runner repo
    
    * docker shell; phony *; rename directory
    
    * add dummy data file
    
    * fix makefile task names; minor edits
    
    * test setup with dummy data file
    
    * makefile task names/expand docker flags
    
    * rename package
    
    * setup.py update
    
    * gha trigger + reusable workflow
    
    * add debug trigger
    
    * checkout + test file locations
    
    * gha in correct dir; delete dummy file
    
    * makefile cleanup; new task naming convention
    
    * cont.
    
    * more makefile reorg
    
    * add gha for build docker images + compile + build wheel
    
    * add assets; fix erronous command in compile step
    
    * fix asset location; minor makefile adjustements
    
    * set common default directory + shell in gha yaml
    
    * fix default directory
    
    * publish output wheel file as Github build artifact
    
    * fix path to wheel file for gha artifact
    
    * remove aux test file
    
    * do not place zip inside wheel (which is already zip)
    
    * ensure output directory structure is preserved
    
    * remove debug trigger

---
+++ mlflow/server/js/webui-py-package/setup.py
@@ -0,0 +1,70 @@
+from typing import Any, List, Tuple
+from pathlib import Path
+
+from setuptools import setup, find_packages
+
+PACKAGE_NAME = "pynb_dag_runner_webui"
+PACKAGE_VERSION = "0.0.0"
+
+OutputDirectoryPath = Any
+InputFilepath = Any
+
+def _list_assets_files(assets: Path) -> List[Tuple[OutputDirectoryPath, List[InputFilepath]]]:
+    """
+    Return list of (directory names, list of file names in directory)
+    data file assets to include in package.
+
+    - Directory names are listed relative to provided `assets`. These
+      paths are used for writing files into the wheel directory structure.
+
+    - Filenames are listed relative to current working directory
+      (assumted to be location of setup.py). These are used to
+      read files from local disk when creating the wheel file.
+
+    - Thus, this makes it possible to reorganize the directory structure,
+      but filenames can not be modified.
+
+    The complexity of the below arises since Python setup does not
+    seem to support "include this directory tree".  See,
+
+    https://docs.python.org/3/distutils/setupscript.html#installing-additional-files
+    """
+
+    if not assets.is_dir():
+        raise Exception(
+            f"Please add files under {assets} directory before building "
+            "{PACKAGE_NAME}!"
+        )
+
+    dir_to_list_of_files_dict = {}
+    for f in assets.glob("**/*"):
+        if f.is_file():
+            dir_name: str = str(f.relative_to(assets).parent)
+            full_filename: str = str(f)
+
+            if dir_name in dir_to_list_of_files_dict:
+                dir_to_list_of_files_dict[dir_name].append(full_filename)
+            else:
+                dir_to_list_of_files_dict[dir_name] = [full_filename]
+
+    return list(dir_to_list_of_files_dict.items())
+
+
+setup(
+    name=PACKAGE_NAME,
+    author="Matias Dahl (based on a modified version of the MLFlow project)",
+    description=(
+        "This Python package contain compiled Javascript code from "
+        "various sources and with different licenses; "
+        "For details, please see the linked documentation and source repo "
+        "https://github.com/pynb-dag-runner/mlflow and "
+        "https://github.com/pynb-dag-runner/mlflow/pull/2"
+    ),
+    author_email="matias.dahl@iki.fi",
+    license="Various, see the description",
+    classifiers=[],
+    url="https://pynb-dag-runner.github.io/pynb-dag-runner/",
+    version=PACKAGE_VERSION,
+    packages=find_packages(),
+    data_files=_list_assets_files(assets = Path("./assets")),
+)
